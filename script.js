6ba3baa7453ad3dfba06a7584204179caffaf527
Oliveredelstein <Oedelstein28@brophybroncos.org>
2024-10-24 01:09:30 +0000

    ef

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var factList = [
+var factList = [/*everything underthis that has facts, will give you 4 facts*/ 
  "Fact 1",
  "Fact 2", 
  "Fact 3", 
  "Fact 4"];
 
-var fact = document.getElementById("fact");
-var myBtn = document.getElementById("myBtn");
-var count = 0;
+var fact = document.getElementById("fact");/*this shows the facts as variables*/ 
+var myBtn = document.getElementById("myBtn");/*this shows the facts as variables*/ 
+var count = 0;/*this shows the facts as variables*/ 
 
 myBtn.addEventListener("click", displayFact);
 
@@ -14,6 +14,6 @@ function displayFact(){
   fact.innerHTML = factList[count];
   count++;
   if (count == factList.length){
-    count = 0;
+    count = 0;/*this code restarts the code back to 0*/
   }
 }
